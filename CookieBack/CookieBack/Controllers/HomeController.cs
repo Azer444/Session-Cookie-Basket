ba84961ee0d0fbcbe96e5acccc961b6d8e36aa6c
Azer444 <azerrh@code.edu.az>
2022-10-13 00:57:04 +0400

    Session-Cookie

---
+++ CookieBack/CookieBack/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CookieBack.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+
+            HttpContext.Session.SetString("Name", "Azer");
+            Response.Cookies.Append("Surname", "Humbetov", new CookieOptions { MaxAge = TimeSpan.FromSeconds(30) });
+            return View();
+
+        }
+        public IActionResult Test()
+        {
+            var sessionData = HttpContext.Session.GetString("Name");
+            var cookieData = Request.Cookies["Surname"];
+            return Json(sessionData + " - " + cookieData);
+        }
+
+        public async Task<IActionResult> AddBasket()
+        {
+            return Json("SalamP130");
+        }
+    }
+}
